732938e59f3fdd37a49eeeb6603f905582ee92d4
emilshadiq06 <emilshadiq@gmail.com>
2025-04-19 22:35:46 +0800

    init
    added theme to panel

--- scene/menuscene/main_menu.gd
+++ scene/menuscene/main_menu.gd
@@ -9,19 +9,22 @@ func _on_exit_pressed() -> void:
 
 
 func _on_start_pressed():
-	$AudioStreamPlayer2D2.play()
+	$click.play()
 	Function.load_screen_to_scene("res://scene/game.tscn")
 
 
 func _on_customize_pressed() -> void:
-	$AudioStreamPlayer2D2.play()
+	$click.play()
 	custom()
 	
 
 func _on_setting_pressed() -> void:
-	$AudioStreamPlayer2D2.play()
+	$click.play()
 	settings()
 	
+func _on_back_pressed() -> void:
+	main()
+
 func main():
 	$main.show()
 	$back.hide()
@@ -33,13 +36,17 @@ func custom():
 	$back.show()
 	$custom.show()
 	$settings.hide()
+	tween_pop($custom)
 	
 func settings():
 	$main.hide()
 	$back.show()
 	$custom.hide()
 	$settings.show()
+	tween_pop($settings)
 
-
-func _on_back_pressed() -> void:
-	main()
+func tween_pop(panel):
+	$zoom.play()
+	panel.scale = Vector2(0.85,0.85)
+	var tween = get_tree().create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_ELASTIC)
+	tween.tween_property(panel, "scale", Vector2(1,1), 0.5)
