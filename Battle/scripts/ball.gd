94d4c70612099b049560560b791856ee88e09c0f
HarunG-yg1 <harun.ghazari@gmail.com>
2025-04-24 01:58:21 +0800

    Adding an attack system that is rythm based
    In progress. Still need to actually replace placeholder

---
+++ Battle/scripts/ball.gd
@@ -0,0 +1,18 @@
+extends AnimatableBody2D
+var move = Vector2.LEFT
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+
+func _physics_process(delta: float) -> void:
+	
+	
+	global_position.x = global_position.x
+	
+		
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
