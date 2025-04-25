a8d397e72aaf26271db274727802d8e2bc53cafb
emilshadiq06 <emilshadiq@gmail.com>
2025-04-25 17:12:53 +0800

    init
    settings menu

--- scene/menuscene/main_menu.gd
+++ scene/menuscene/main_menu.gd
@@ -68,3 +68,21 @@ func _on_setting_mouse_entered() -> void:
 
 func _on_exit_mouse_entered() -> void:
 	$hover.play()
+
+
+func _on_h_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(0,value)
+
+
+func _on_mute_toggled(toggled_on: bool) -> void:
+	AudioServer.set_bus_mute(0,toggled_on)
+
+
+func _on_resolution_item_selected(index: int) -> void:
+	match index:
+		0:
+			DisplayServer.window_set_size(Vector2i(1920, 1080))
+		1:
+			DisplayServer.window_set_size(Vector2i(1600, 900))
+		2:
+			DisplayServer.window_set_size(Vector2i(1280, 720))
