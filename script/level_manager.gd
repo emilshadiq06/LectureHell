7fd4c366d3c15242595244ebd77abe8c8136d85a
emilshadiq06 <emilshadiq@gmail.com>
2025-05-18 21:06:33 +0800

    init
    boss spawn now

--- script/level_manager.gd
+++ script/level_manager.gd
@@ -3,6 +3,9 @@ extends Node
 @onready var player: Character = $"../Player" 
 @onready var ui: CanvasLayer = $"../UI"
 @onready var wave_spawner: WaveSpawner = $"../WaveSpawner"
+@onready var enemy_movement_points: Node = $"../EnemyMovementPoints"
+
+const VLAD_BOSS = preload("res://scene/vlad_boss.tscn")
 
 func _ready() -> void:
 	ui.set_initial_health(player.get_health())
@@ -11,7 +14,13 @@ func _ready() -> void:
 	wave_spawner.waves_finished.connect(on_waves_finished)
 	
 func on_waves_finished():
-	pass
+	var vlad_boss = VLAD_BOSS.instantiate()
+	get_tree().root.add_child(vlad_boss)
+	
+	vlad_boss.global_position = Vector2(1116, 310)
+	vlad_boss.movement_points = enemy_movement_points.get_children()
+	vlad_boss.init()
+	
 
 func on_player_damaged(current_health: int):
 	ui.decrease_health(current_health)
--- script/vlad_boss.gd
+++ script/vlad_boss.gd
@@ -1,11 +1,48 @@
 extends Area2D
 
+signal vlad_damage(current_health: int)
+signal vlad_died
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var speed = 300
+var movement_points
+var current_movement_point
+
+enum PHASE {
+	ONE,
+	TWO
+}
+
+enum Action {
+	SPAWN_DEVIL,
+	HOMING_SHOT,
+	SPRAY_SHOT
+}
+
+var phase = PHASE.ONE
+
+@onready var health_system: HealthSystem = $HealthSystem
+@onready var shooting_point: Marker2D = $ShootingPoint
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var action_timer: RandomTimer = $ActionTimer
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func init():
+	var random_point = movement_points.pick_random()
+	current_movement_point = random_point.position
+	health_system.damaged.connect(on_damaged)
+	health_system.died.connect(on_died)
+	action_timer.setup()
+	
+func on_died():
+	vlad_died.emit()
+
+func on_damaged(current_health: int):
+	vlad_damage.emit(current_health)
+	
 func _process(delta: float) -> void:
-	pass
+	global_position = global_position.move_toward(current_movement_point, delta * speed)
+	
+	if global_position.distance_squared_to(current_movement_point) < .1:
+		current_movement_point = movement_points.pick_random().global_position
+	
+	
