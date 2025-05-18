da37dfbe85c10cbfc0ee7ceb1df6c4a472f541aa
emilshadiq06 <emilshadiq@gmail.com>
2025-05-18 20:49:34 +0800

    init
    boss making

--- script/level_manager.gd
+++ script/level_manager.gd
@@ -8,6 +8,10 @@ func _ready() -> void:
 	ui.set_initial_health(player.get_health())
 	player.player_damaged.connect(on_player_damaged)
 	wave_spawner.starting_wave.connect(ui.on_wave_started)
+	wave_spawner.waves_finished.connect(on_waves_finished)
+	
+func on_waves_finished():
+	pass
 
 func on_player_damaged(current_health: int):
 	ui.decrease_health(current_health)
