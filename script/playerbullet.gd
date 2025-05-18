cb82b7b83b6774d7926c6fd467e15f1e704710c0
emilshadiq06 <emilshadiq@gmail.com>
2025-05-18 15:14:51 +0800

    init
    player can change hehe

--- script/playerbullet.gd
+++ script/playerbullet.gd
@@ -1,11 +1,46 @@
 extends Area2D
 
+class_name Character
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+@export var speed = 400
+var direction
+
+var animation_prefix
+
+
 func _process(delta: float) -> void:
-	pass
+	animation_prefix = GameConfigBullet.PlayerType.keys()[GameConfigBullet.player_type].to_snake_case()
+	animated_sprite_2d.play("%s_default" % animation_prefix)
+	var next_position = position + direction * speed * delta
+	if !is_within_screen_bounds(next_position):
+		return
+	position = next_position
+
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("up"):
+		direction = Vector2.UP
+	elif Input.is_action_just_pressed("down"):
+		direction = Vector2.DOWN
+	elif Input.is_action_just_pressed("left"):
+		direction = Vector2.LEFT
+	elif Input.is_action_just_pressed("right"):
+		direction = Vector2.RIGHT
+	else:
+		direction = Vector2.ZERO
+		
+func is_within_screen_bounds(next_position: Vector2):
+	var half_size = collision_shape_2d.shape.get_rect().size / 2
+	
+	var viewport_size = get_viewport_rect().size
+	
+	if next_position.y > half_size.y && next_position.y + half_size.y < viewport_size.y && next_position.x > half_size.x && next_position.x + half_size.x < viewport_size.x:
+		return true
+	
+	return false
+	
+		
+	
