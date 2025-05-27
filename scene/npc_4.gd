102bf22392dd8c8435515099058598e0cc19f73b
HarunG-yg1 <harun.ghazari@gmail.com>
2025-05-28 04:31:26 +0800

    npc4 changed speed

--- scene/npc_4.gd
+++ scene/npc_4.gd
@@ -1,13 +1,17 @@
 extends CharacterBody2D
 
-var speed = 60
+var speed =100
 var player_chase = false
 var player = null
-
+var direction : Vector2
 
 func _physics_process(delta):
+	move_and_slide()
 	if player_chase:
-		position += (player.position - position)/speed
+		direction = (player.position - position).normalized()
+		velocity = direction * speed
+		#print(direction)
+		print(velocity)
 		
 		$AnimatedSprite2D.play("walk_e")
 		
@@ -26,4 +30,6 @@ func _on_detection_area_body_entered(body: Node2D) -> void:
 
 func _on_detection_area_body_exited(body: Node2D) -> void:
 	player = null
+	direction = Vector2.ZERO
+	velocity = Vector2.ZERO
 	player_chase = false
