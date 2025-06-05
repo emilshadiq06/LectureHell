faf74fc342cc8dd5ef97b169d5ad802be56267b9
IrfanZikry-69 <irfanzikry1113@gmail.com>
2025-06-05 22:31:08 +0800

    added collectable item and new dialogue for character

---
+++ gas_collectable.gd
@@ -0,0 +1,15 @@
+extends StaticBody2D
+
+@export var item: InvItem
+var player = null
+
+
+func _on_interactable_area_body_entered(body: Node2D) -> void:
+	if body is Player:
+		player = body
+		playercollect()
+		await get_tree().create_timer(0.1).timeout
+		self.queue_free()
+		
+func playercollect():
+	player.collect_item(item)
